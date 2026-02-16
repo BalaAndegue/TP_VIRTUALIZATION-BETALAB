e2e5c0bf6ed11a7eab72c8785cf7f82df80167ff
Bala Andegue <balaandeguefrancoislionnel@gmail.com>
2026-02-16 02:34:08 +0100

    clean remove unusefull files

--- load_manager/loader.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LOADER__
-#define __LOADER__
-
-#include <stdio.h>
-#include <stdint.h>
-#include <linux/kvm.h>
-#include "../vm_manager/manager.h"
-int load_vm_code(const uint8_t *code);
-#endif
