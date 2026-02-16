d2e9806f8caa49ec38df3b293e0d43a784bc1151
Bala Andegue <balaandeguefrancoislionnel@gmail.com>
2026-02-16 03:41:09 +0100

    load_vm_code done

--- lab_1/load_manager/loader.c
+++ lab_1/load_manager/loader.c
@@ -5,4 +5,11 @@
  */
 int load_vm_code(const uint8_t *code)
 {
+    // On récupère le pointeur vers le début de la RAM de la VM
+    uint8_t *guest_mem = get_memory();
+    
+    // On copie les instructions binaires (le tableau code[]) au tout début (adresse 0)
+    // Nous copions environ 16 octets qui correspondent à la taille du binaire fourni dans main.c
+    memcpy(guest_mem, code, 16); [cite_start]// [cite: 6]
+    return 0;
 }
